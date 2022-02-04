0c21e35a9a198bb28855e732b3b11433d0c05dab
Adilson Lima <adilson.pereira.lima@gmail.com>
2022-02-04 11:27:10 -0300

    first commit

---
+++ lib/plate_slate_web/schema.ex
@@ -0,0 +1,12 @@
+defmodule PlateStateWeb.Schema do
+  use Absinthe.Schema
+
+  query do
+  end
+
+  object :menu_item do
+    field :id, :id
+    field :name, :string
+    field :description, :string
+  end
+end
